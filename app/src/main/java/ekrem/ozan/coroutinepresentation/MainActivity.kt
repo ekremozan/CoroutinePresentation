26e2096596bee4dac419fa41bf1c7b6b3dfb2500
ekremozanuysal <ekremozanuysal@gmail.com>
2022-02-27 23:02:54 +0300

    First coroutine sample was added

--- app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
+++ app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
@@ -2,6 +2,10 @@ package ekrem.ozan.coroutinepresentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
@@ -9,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            delay(2000L)
+            Log.d(TAG, "Coroutine... ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Main... ${Thread.currentThread().name}")
     }
 }
