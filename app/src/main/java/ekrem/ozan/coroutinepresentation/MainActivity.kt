c9b9d7fa85991d9356392d7e65dd0a1d5e5a6f08
ekremozanuysal <ekremozanuysal@gmail.com>
2022-02-27 23:55:11 +0300

    Job cancellation with isActive condition sample was added

--- app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
+++ app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package ekrem.ozan.coroutinepresentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
@@ -9,5 +11,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timer = GlobalScope.launch(Dispatchers.Default) {
+            for (i in 35..45) {
+                if (isActive)
+                    Log.d(TAG, "Result for $i: ${fib(i)}")
+            }
+            Log.d(TAG, "Ending long running process....")
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(300L)
+            timer.cancel()
+            Log.d(TAG, "Main thread is continuing....")
+        }
     }
+
+    private fun fib(n: Int): Long {
+        return if (n == 0) 0
+        else if (n == 1) 1
+        else fib(n - 1) + fib(n - 2)
+    }
+
 }
