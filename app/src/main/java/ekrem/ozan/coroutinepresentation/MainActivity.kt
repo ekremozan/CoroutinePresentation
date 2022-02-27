ee090ff44e135d4a6e5cc9a684eec736f1675493
ekremozanuysal <ekremozanuysal@gmail.com>
2022-02-27 23:00:26 +0300

    Tag was added

--- app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
+++ app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
