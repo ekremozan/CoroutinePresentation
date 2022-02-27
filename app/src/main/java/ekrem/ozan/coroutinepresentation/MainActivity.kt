f96bb256793de5eb526a4e44a35aa9b2a27e16c2
ekremozanuysal <ekremozanuysal@gmail.com>
2022-02-28 00:24:24 +0300

    Async and await sample was added

--- app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
+++ app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
@@ -1,7 +1,10 @@
 package ekrem.ozan.coroutinepresentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
@@ -9,5 +12,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val time = measureTimeMillis {
+                val firstResponse = async { networkCallOne() }
+                val secondResponse = async { networkCallTwo() }
+
+                Log.d(TAG, "AnswerOne is: ${firstResponse.await()}")
+                Log.d(TAG, "AnswerTwo is: ${secondResponse.await()}")
+
+            }
+            Log.d(TAG, "Requests took $time ms.")
+        }
+    }
+
+    private suspend fun networkCallOne(): String {
+        delay(2000L)
+        return "Answer 1"
+    }
+
+    private suspend fun networkCallTwo(): String {
+        delay(3000L)
+        return "Answer 2"
     }
 }
