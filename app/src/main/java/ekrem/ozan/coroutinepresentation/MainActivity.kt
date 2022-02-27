8af7dcd414d82dac55bd724aa0f46bf530950414
ekremozanuysal <ekremozanuysal@gmail.com>
2022-02-27 23:57:58 +0300

    Job cancellation sample was added

--- app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
+++ app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
@@ -2,6 +2,11 @@ package ekrem.ozan.coroutinepresentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
@@ -9,5 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timer = GlobalScope.launch(Dispatchers.Default) {
+            repeat(5) {
+                Log.d(TAG, "Coroutine is still working....${it}")
+                delay(1000L)
+            }
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(2000L)
+            timer.cancel()
+            Log.d(TAG, "Main thread is continuing....")
+        }
     }
+
 }
