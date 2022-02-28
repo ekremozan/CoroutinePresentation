b1749745c6b2b7115e62112320a330183fee82aa
ekremozanuysal <ekremozanuysal@gmail.com>
2022-02-28 23:51:48 +0300

    Lifecycle and view model scopes

--- app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
+++ app/src/main/java/ekrem/ozan/coroutinepresentation/MainActivity.kt
@@ -1,7 +1,14 @@
 package ekrem.ozan.coroutinepresentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
@@ -9,5 +16,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnOtherActivity)
+
+        button.setOnClickListener {
+            lifecycleScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(TAG, "Still running...")
+                }
+            }
+
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
+
+
     }
 }
